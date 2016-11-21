015de8ea25d6b1d4d8e0216c083939f94696f2df
xqz <1106617086@qq.com>
2016-11-21 23:27:43 +0800

    增加后台自动更新天气

--- src/com/coolweather/app/activity/WeatherActivity.java
+++ src/com/coolweather/app/activity/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.coolweather.app.activity;
 
+import com.coolweather.app.server.AutoUpdateService;
 import com.coolweather.app.util.HttpCallbackListener;
 import com.coolweather.app.util.HttpUtil;
 import com.coolweather.app.util.Utility;
@@ -171,15 +172,19 @@ public class WeatherActivity extends Activity implements OnClickListener {
 	 * 从SharedPreferences文件中读取存储的天气信息，并显示到界面上。
 	 */
 	private void showWeather() {
-		SharedPreferences prefs = PreferenceManager
-				.getDefaultSharedPreferences(this);
-		cityNameText.setText(prefs.getString("city_name", ""));
-		temp1Text.setText(prefs.getString("temp1", ""));
-		temp2Text.setText(prefs.getString("temp2", ""));
-		weatherDespText.setText(prefs.getString("weather_desp", ""));
-		publishText.setText("今天" + prefs.getString("publish_time", "") + "发布");
-		currentDateText.setText(prefs.getString("current_date", ""));
-		weatherInfoLayout.setVisibility(View.VISIBLE);
-		cityNameText.setVisibility(View.VISIBLE);
+		SharedPreferences prefs = PreferenceManager.
+				getDefaultSharedPreferences(this);
+				cityNameText.setText( prefs.getString("city_name", ""));
+				temp1Text.setText(prefs.getString("temp1", ""));
+				temp2Text.setText(prefs.getString("temp2", ""));
+				weatherDespText.setText(prefs.getString("weather_desp", ""));
+				publishText.setText("今天" + prefs.getString("publish_time", "") +
+				"发布");
+				currentDateText.setText(prefs.getString("current_date", ""));
+				weatherInfoLayout.setVisibility(View.VISIBLE);
+				cityNameText.setVisibility(View.VISIBLE);
+				Intent intent = new Intent(this, AutoUpdateService.class);
+				startService(intent);
+				}
 	}
-}
+
